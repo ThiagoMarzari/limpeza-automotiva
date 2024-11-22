b4733cc2adc3aaa7da9684e0906a4bd524aa0fd1
ThiagoMarzari <87370358+ThiagoMarzari@users.noreply.github.com>
2024-11-22 08:57:17 -0300

    Update firebase.ts

--- car-washh/src/services/firebase.ts
+++ car-washh/src/services/firebase.ts
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBgGvZo7laG7cJ5TjzMfVIt60oJmxq0nEY",
-    authDomain: "car-wash-e5633.firebaseapp.com",
-    projectId: "car-wash-e5633",
-    storageBucket: "car-wash-e5633.firebasestorage.app",
-    messagingSenderId: "141709780427",
-    appId: "1:141709780427:web:130b4669da24589565424e"
+    //adicionar aqui sua chave de api
   };
 
 
